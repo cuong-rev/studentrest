5ddb12c52385f634e50576a44a716a8453c87c94
cuong <c.nguyenviet@gmail.com>
2021-10-28 10:59:51 +0200

    delete/revert back to begin

--- src/main/java/com/se/studentrest/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.se.studentrest.repository;
-
-import com.se.studentrest.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
