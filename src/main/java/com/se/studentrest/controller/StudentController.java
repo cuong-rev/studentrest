0b7470a8f78f729cb01a71fcd0f5fc646ccbc9b8
cuong <c.nguyenviet@gmail.com>
2021-11-04 08:47:39 +0100

    end lab 2

--- src/main/java/com/se/studentrest/controller/StudentController.java
+++ src/main/java/com/se/studentrest/controller/StudentController.java
@@ -2,9 +2,12 @@ package com.se.studentrest.controller;
 
 import com.se.studentrest.entity.Student;
 import com.se.studentrest.repository.StudentRepository;
+import com.se.studentrest.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +22,17 @@ public class StudentController {
     @GetMapping("/student")
     public List<Student> getAll() {
         return studentRepository.findAll();
+    }
+
+    @PostMapping("/student")
+    public String saveStudent(@RequestBody Student student) {
+        String password = student.getPassword();
+
+        // Hash the password
+        String hashedString = SecurityUtils.hashPassword(password);
+        student.setPassword(hashedString);
+        studentRepository.save(student);
+        return "Success";
     }
 
 }
